c830604448ce61cc4412a420fb4e686fae33d082
apokrif6 <85841612+apokrif6@users.noreply.github.com>
2023-10-24 10:03:46 +0200

    Create 35_search_insert_position.cpp

---
+++ 35_search_insert_position.cpp
@@ -0,0 +1,24 @@
+//Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+//You must write an algorithm with O(log n) runtime complexity.
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int left = 0;
+        int right = nums.size() - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[mid] < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+
+        return left;
+    }
+};
